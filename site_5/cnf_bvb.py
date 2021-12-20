87a14755b23b4824fd10c84571fd8329c92151e7
GH0STAV0 <you@example.com>
2021-12-20 10:13:33 -0600

    add new project

--- site_5/cnf_bvb.py
+++ site_5/cnf_bvb.py
@@ -3,14 +3,14 @@ import json
 import socket
 
 
-url_1="https://www.it-the-3h.nl.eu.org/index.html"
-# url_1="http://dark-market.ueuo.com/index.html"
+# url_1="https://www.it-the-3h.nl.eu.org/index.html"
+url_1="http://dark-market.ueuo.com/index.html"
 
-url_2=url_1.replace("https://","")
+url_2=url_1.replace("http://","")
 url_2=url_2.replace("/index.html","")
 
 # N= norrd
-vpn_type="N"
+vpn_type="C"
 # vpn_type="C" b
 
 
