78b6aa6c3bc91a5908e2de92aa746a30d1acdc99
GH0STAV0 <you@example.com>
2021-12-20 07:57:44 -0600

    add new project

--- site_5/cnf_bvb.py
+++ site_5/cnf_bvb.py
@@ -3,7 +3,8 @@ import json
 import socket
 
 
-url_1="https://www.it-the-3h.nl.eu.org/index.html"
+# url_1="https://www.it-the-3h.nl.eu.org/index.html"
+url_1="https://dark-market.ueuo.com/index.html"
 
 url_2=url_1.replace("https://","")
 url_2=url_2.replace("/index.html","")
