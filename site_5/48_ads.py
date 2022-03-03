adc94f6ccdc5867e5221a95b3a62437a6bf8edbd
GH0STAV0 <you@example.com>
2022-03-03 15:01:42 +0100

    add new project

--- site_5/48_ads.py
+++ site_5/48_ads.py
@@ -213,8 +213,8 @@ def ads_class(driver):
 
 
 		# cnf_bvb.send_msg("oooooooooooooooooooo")
-		driver.refresh()
-		time.sleep(15)
+	driver.refresh()
+	time.sleep(15)
 
 	append_to_l0g("VISITE WEB-SITE [ 2 ] : [ +second_2_visit+]  OK")
 		# time.sleep(2)
