6557ae2d8bbc257a58f7a25f7a425a6ae7d1c38c
GH0STAV0 <you@example.com>
2022-04-24 11:25:54 +0100

    my connecto python

--- main_oct0pus/cnf_bvb.py
+++ main_oct0pus/cnf_bvb.py
@@ -25,10 +25,10 @@ print(read_current_acc_goo())
 # g00g_acc="garmiyashour"
 # 
 # erogomayke
-g00g_acc="erogomayke"
+# g00g_acc="erogomayke"
 
 # g00g_acc="laminewalter7"
-# g00g_acc="dahmandargo"
+g00g_acc="dahmandargo"
 # g00g_acc="xamiramogdan"
 pofile_path=pwd+"/"+g00g_acc
 
