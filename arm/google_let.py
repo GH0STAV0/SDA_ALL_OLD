c836dc66806c39781e88bcf9c6068083e2a0ecca
GH0STAV0 <you@example.com>
2022-11-05 23:55:18 +0100

    add active_account

--- arm/google_let.py
+++ arm/google_let.py
@@ -318,7 +318,7 @@ def starting_tasks():
 		stage_1()### CLEAR
 		# mod_vpn2.fnc_vpn ()
 		visible_v=cnf_bvb.visible_v
-		display = Display(visible=visible_v, size=(width,height)).start()
+		#display = Display(visible=visible_v, size=(width,height)).start()
 		driver=mod_driver2.build_driver(width ,height)
 		lets_play(driver)
 		display.stop()
